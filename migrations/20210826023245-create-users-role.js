f90405e1e09bdb54b1551d6da3bb73d0da02f99f
bkomalram <bhagwan.ram.komalram@gmail.com>
2021-08-25 21:49:39 -0500

    agregando migraciones de tablas de manejo de usuarios

---
+++ migrations/20210826023245-create-users-role.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UsersRoles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      role_id: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UsersRoles');
+  }
+};
---
+++ migrations/20210826023719-migration-foreignkeys.js
@@ -0,0 +1,46 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+      await queryInterface.addConstraint('UsersRoles', {
+        fields: ['user_id'],
+        type: 'foreign key',
+        name: 'fkey_constraint_user_ur',
+        references: { //Required field
+          table: 'Users',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      });
+
+      await queryInterface.addConstraint('UsersRoles', {
+        fields: ['role_id'],
+        type: 'foreign key',
+        name: 'fkey_constraint_role_ur',
+        references: { //Required field
+          table: 'Roles',
+          field: 'id'
+        },
+        onDelete: 'cascade',
+        onUpdate: 'cascade'
+      });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.removeConstraint('UserRoles', 'fkey_constraint_user_ur');
+     await queryInterface.removeConstraint('UserRoles', 'fkey_constraint_role_ur');
+  }
+};
